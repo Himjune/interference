aa4e2d307ed289bf6b5aa32c78a442bcdc5b6731
Himjune <himjune@mail.ru>
2025-04-19 04:55:09 +0300

    save file with flask

---
+++ server.py
@@ -0,0 +1,23 @@
+import os.path
+import json
+from datetime import datetime
+
+from flask import Flask, Response, request, jsonify
+
+from flask import send_from_directory
+
+app = Flask(__name__)
+@app.route('/results/<uuid>', methods=['POST'])
+def results(uuid):
+    with open('file.json', 'w') as f:
+        json.dump(request.json, f)
+
+    return jsonify({"uuid": uuid})
+
+@app.route('/<path:path>')
+def serve_html(path):
+    # Using request args for path will expose you to directory traversal attacks
+    return send_from_directory('static', path)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="80", debug=True)
